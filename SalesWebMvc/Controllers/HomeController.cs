a79535ee6d6b8ff8a9e57bb4cffc4b16a591f7b9
Pedro Môra Neto <pedro_mora_neto@hotmail.com>
2021-01-27 21:01:29 -0300

    First Controller and Razon pages testes

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,8 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Salles Web MVC App from course C#.";
+            ViewData["Aluno"] = "Pedro Môra Neto";
             return View();
         }
 
